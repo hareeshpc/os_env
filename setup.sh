f7f33801f8ee853a413fa9f6047b7095236f30ec
Hareesh Puthalath <hareesh.puthalath@gmail.com>
2017-08-28 13:25:37 -0700

    Backup bash profile and add to current session

--- setup.sh
+++ setup.sh
@@ -10,6 +10,9 @@ git clone https://github.com/hareeshpc/mercury_scripts.git
 
 # Source global definitions
 if [ -f ~/.bash_profile ]; then
+  cp ~/.bash_profile{,.backup}
   echo "PATH=$PATH:/var/hareesh/mercury_scripts" >> ~/.bash_profile
   echo "export PATH" >> ~/.bash_profile
 fi
+echo "Exporting path variable"
+export PATH=$PATH:/var/hareesh/mercury_scripts
